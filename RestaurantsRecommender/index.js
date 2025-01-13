bcae2b2e9f3c48afb2bc75aebe4c81be11f33960
efrontinan <elenauxui@gmail.com>
2025-01-13 14:09:52 +0100

    Solving issues

--- RestaurantsRecommender/index.js
+++ RestaurantsRecommender/index.js
@@ -1,37 +1,31 @@
-import restaurants from './restaurants.ts';
-
-const dollarSigns = '$$';
-const deliveryTimeMax = '90';
-const maxDistance = 10;
-const hour: number = new Date().getHours()
-let result: string;
-
-const priceBracket: number = dollarSigns.length;
-
-const filteredRestaurants = restaurants.filter((restaurant) => {
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+var restaurants_1 = require("./restaurants");
+var dollarSigns = '$$';
+var deliveryTimeMax = '90';
+var maxDistance = 10;
+var hour = new Date().getHours();
+var result;
+var priceBracket = dollarSigns.length;
+var filteredRestaurants = restaurants_1.default.filter(function (restaurant) {
     if (Number(restaurant.priceBracket) > priceBracket) {
         return false;
     }
-
     if (restaurant.deliveryTimeMinutes > Number(deliveryTimeMax)) {
         return false;
     }
-
     if (Number(restaurant.distance) > maxDistance) {
         return false;
     }
-
     if (hour < Number(restaurant.openHour) || hour > Number(restaurant.closeHour)) {
         return false;
     }
-
     return restaurant;
 });
-
 if (filteredRestaurants.length === 0) {
     result = 'There are no restaurants available right now.';
-} else {
-    result = `We found ${filteredRestaurants.length} restaurants, the first is ${filteredRestaurants[0].name}.`;
 }
-
+else {
+    result = "We found ".concat(filteredRestaurants.length, " restaurants, the first is ").concat(filteredRestaurants[0].name, ".");
+}
 console.log(result);
